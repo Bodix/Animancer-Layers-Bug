0111a2b8530730ba903479c423edfaae056ef436
Bodix <bodix321@gmail.com>
2022-04-26 20:24:25 +0300

    Add IK setting

--- Assets/Scripts/SetupAnimancer.cs
+++ Assets/Scripts/SetupAnimancer.cs
@@ -6,13 +6,17 @@ public class SetupAnimancer : MonoBehaviour
     [SerializeField]
     private HybridAnimancerComponent _animancer;
     [SerializeField]
-    private AnimationClip _animation;
+    private ClipTransition _transition;
     [SerializeField]
     private AvatarMask _mask;
 
     private void Start()
     {
         _animancer.Layers[1].SetMask(_mask);
-        _animancer.Layers[1].Play(_animation);
+        // This improves the situation a bit, but it still looks ugly.
+        // _animancer.Layers[1].ApplyAnimatorIK = true;
+        // _animancer.Layers[1].ApplyFootIK = true;
+        
+        _animancer.Layers[1].Play(_transition);
     }
 }
